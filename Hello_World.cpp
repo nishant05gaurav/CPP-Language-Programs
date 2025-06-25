37dcb611a09fd25bc6398e918c295ced09b2482f
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2025-06-25 22:16:37 +0530

    first commit

---
+++ Hello_World.cpp
@@ -0,0 +1,12 @@
+// Question: https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+// Solution:
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
